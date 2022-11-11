a4e50b57358c4d34f301040559982786b95e8b70
Aida1108 <mi.aida@yandex.ru>
2022-11-11 20:27:23 +0500

    Задание 8

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
+
+        // Задача 1
+        System.out.println("Задача 1");
+        var dog = 8;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
         var paper = 763789;
         System.out.println(paper);
+
+        // Задача 2
+        System.out.println("Задача 2");
         dog = dog + 4;
         System.out.println(dog);
         cat = cat + 4;
         System.out.println(cat);
         paper = paper + 4;
         System.out.println(paper);
-        dog = dog - 3.5;
-        System.out.println(dog);
+
+        // Задача 3
+        System.out.println("Задача 3");
+        var dog1=8.0;
+        dog1= dog1 - 3.5;
+        System.out.println(dog1);
         cat = cat - 1.6;
         System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
+
+        // Задание 4
+        System.out.println("Задание 4");
         var friend = 19;
         System.out.println(friend);
-        friend = friend * 2;
+        friend = friend + 2;
         System.out.println(friend);
-        friend = friend * 7;
+        friend = friend / 7;
         System.out.println(friend);
+
+        // Задание 5
+        System.out.println("Задание 5");
         var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
@@ -32,16 +48,28 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
-        var oneBoxer = 78.2;
-        var twoBoxer = 82.7;
-        var sumBoxer = oneBoxer + twoBoxer;
-        System.out.println("Oбщий вес двух бойцов " + sumBoxer + " кг");
-        var differenceBoxer = twoBoxer - oneBoxer;
-        System.out.println("Разницу между весами бойцов " + differenceBoxer + " кг");
-        var subtractionBoxer = twoBoxer - oneBoxer;
-        System.out.println("Вычитание из большего веса меньшего " + subtractionBoxer + " кг");
-        var remainsBpxer = twoBoxer % oneBoxer;
-        System.out.println("Используя функцию остаток от деления " + remainsBpxer + " кг");
+
+        // Задание 6
+        System.out.println("Задание 6");
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        System.out.println("Oбщий вес двух бойцов: " + (boxer1Weight + boxer2Weight) + " кг");
+        System.out.println("Разница весов двух бойцов: " + (boxer2Weight - boxer1Weight) + " кг");
+
+        // Задание 7
+        System.out.println("Задание 7");
+        System.out.println("Разница весов двух бойцов (1 способ): " + (boxer2Weight - boxer1Weight) + " кг");
+        System.out.println("Разница весов двух бойцов (2 способ): " + (boxer2Weight - boxer1Weight) + " кг");
+
+        // Задание 8
+        System.out.println("Задание 8");
+        var totalWorkingHours = 640;
+        var hoursPerDay = 8;
+        var totalEmployees = totalWorkingHours / hoursPerDay;
+        System.out.println("Всего работников в компании – " + totalEmployees + " человек");
+        totalEmployees = totalEmployees + 94;
+        totalWorkingHours = totalEmployees * hoursPerDay;
+        System.out.println("Если в компании работает  " + totalEmployees + " человека, то всего " + totalWorkingHours + " часа работы может быть поделено между сотрудниками");
 
 
 
